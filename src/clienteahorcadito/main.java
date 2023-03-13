5c138bd190f49fea7bc51246b5f74e2a79727782
sarau26 <sara.uribez@udea.edu.co>
2023-03-13 10:43:25 -0500

    Primer commit

---
+++ src/clienteahorcadito/main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clienteahorcadito;
+
+import vista.ini;
+
+/**
+ *
+ * @author Sara Uribe
+ */
+public class main {
+    public static void main(String[] args) {
+        
+        ini ventanita = new ini();
+        ventanita.setLocationRelativeTo(null);
+        ventanita.setVisible(true);
+         
+    }
+}
